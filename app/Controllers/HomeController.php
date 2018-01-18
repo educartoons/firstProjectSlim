077e8a2bc9fca85d9c8a624d87a6f34aa9766661
Eduar Apaza Yáñez <eduarapazayanez@Eduars-MacBook-Pro.local>
2018-01-17 19:27:45 -0500

    My first view renderd by Twig

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,19 +4,13 @@ namespace App\Controllers;
 
 use Slim\Views\Twig as View;
 
-class HomeController
+class HomeController extends Controller
 {
-
-  protected $view;
-
-  public function __construct(View $view)
-  {
-    $this->view = $view;
-  }
 
   public function index($request, $response)
   {
     //return 'It is the home';
-    return $this->view->render($response, 'home.twig');
+    return $this->container->view->render($response, 'home.twig');
   }
+
 }
